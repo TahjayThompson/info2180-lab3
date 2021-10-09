83a04426e22bd2b5cf9fdc5ffdd0bb59b3f118a0
Tahjay Thompson <thompsontahjay34@gmail.com>
2021-10-08 22:35:35 -0500

    added cheating pol and winner

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,74 +2,90 @@
 
 window.onload = function() {
 
-const game = document.getElementById("game");
-const status = document.getElementById("status");
-const board = document.getElementById("board");
-const controls = document.getElementsByClassName("controls")[0];
-const button = document.getElementsByClassName("btn")[0];
+    const game = document.getElementById("game");
+    const status = document.getElementById("status");
+    const board = document.getElementById("board");
+    const controls = document.getElementsByClassName("controls")[0];
+    const button = document.getElementsByClassName("btn")[0];
 
-const divs = board.querySelectorAll('div');
-let plays = [null,null,null,null,null,null,null,null,null];
-let clicke = true;
-let play ="X";
-
-
-divs.forEach((elem,index)=>{
-    elem.setAttribute("class","square");
-    console.log(elem,index)
-
-    elem.addEventListener('click',function(){
-        if(clicke){
-            clicke=false;
-        }
-        else{
-            clicke=true;
-        }
+    const elem = board.querySelectorAll('div');
+    let plays = [null,null,null,null,null,null,null,null,null];
+    let play ="X";
 
 
-        plays[index]=play;
-        elem.innerHTML=play;
-        elem.classList.add(play);
+    for(let i=0; i<=8;i++){
+       elem[i].setAttribute("class","square");
 
+           elem[i].addEventListener('click',function(){
+                // plays[i]=play;
+                elem[i].innerHTML=play;
+                elem[i].classList.add(play);
 
-        if(play=="O"){
-            play='X';
-            plays[index]=play;
-            elem.innerHTML=play;
-            elem.classList.add(play);
+                if(play=="O"){
+                    play='X';
+                    // plays[i]=play;
+                    elem[i].innerHTML=play;
+                    elem[i].classList.add(play);
+            
+                }
+                else{
+                    play="O";
+                    // plays[i]=play;
+                    elem[i].innerHTML=play;
+                    elem[i].classList.add(play);
+                }
+                elem[i].style.pointerEvents = 'none';
+
+                if (
+                    elem[0].innerHTML=="X" && elem[1].innerHTML=="X" && elem[2].innerHTML=="X" || 
+                    elem[3].innerHTML=="X" && elem[4].innerHTML=="X" && elem[5].innerHTML=="X" ||
+                    elem[6].innerHTML=="X" && elem[7].innerHTML=="X" && elem[8].innerHTML=="X" || 
+                    elem[0].innerHTML=="X" && elem[3].innerHTML=="X" && elem[6].innerHTML=="X" || 
+                    elem[1].innerHTML=="X" && elem[4].innerHTML=="X" && elem[7].innerHTML=="X" || 
+                    elem[2].innerHTML=="X" && elem[5].innerHTML=="X" && elem[8].innerHTML=="X" || 
+                    elem[2].innerHTML=="X" && elem[4].innerHTML=="X" && elem[6].innerHTML=="X" || 
+                    elem[0].innerHTML=="X" && elem[4].innerHTML=="X" && elem[8].innerHTML=="X" ){
+                    status.innerHTML='Congratulations! X is the Winner!"'
+                    status.classList.add('you-won');
+
+                }
+                else if(
+                    elem[0].innerHTML=="O" && elem[1].innerHTML=="O" && elem[2].innerHTML=="O" || 
+                    elem[3].innerHTML=="O" && elem[4].innerHTML=="O" && elem[5].innerHTML=="O" ||
+                    elem[6].innerHTML=="O" && elem[7].innerHTML=="O" && elem[8].innerHTML=="O" ||
+                    elem[0].innerHTML=="O" && elem[3].innerHTML=="O" && elem[6].innerHTML =="O" ||
+                    elem[1].innerHTML=="O" && elem[4].innerHTML=="O" && elem[7].innerHTML=="O" ||
+                    elem[2].innerHTML=="O" && elem[5].innerHTML=="O" && elem[8].innerHTML=="O" ||
+                    elem[2].innerHTML=="O" && elem[4].innerHTML=="O" && elem[6].innerHTML=="O" ||
+                    elem[0].innerHTML=="O" && elem[4].innerHTML=="O" && elem[8].innerHTML=="O" ){
+                    status.innerHTML='Congratulations! O is the Winner!"'
+                    status.classList.add('you-won');
+            
+                }
+                else{
+                }
+            console.log(elem[3].innerHTML =="O")
+               
+
+            });
+
+            
+
+           elem[i].addEventListener('mouseover',function(){
+               elem[i].classList.add('hover');
+
+            });
+           elem[i].addEventListener('mouseout',function(){
+               elem[i].classList.remove('hover');
+
+            });
+    }
+    console.log(plays);
+
     
-        }
-        else{
-            play="O";
-            plays[index]=play;
-            elem.innerHTML=play;
-            elem.classList.add(play);
-    
-        }
-
-        console.log(clicke)
-        console.log(plays)
-    });
-
-    elem.addEventListener('mouseover',function(){
-        elem.classList.add('hover');
-
-    });
-    elem.addEventListener('mouseout',function(){
-        elem.classList.remove('hover');
-
-    });
-
- 
-});
 
 
 
 
-console.log(divs)
-console.log(game)
-console.log(status)
-console.log(board)
-console.log(controls)
-console.log(button)
-}
+};
+
